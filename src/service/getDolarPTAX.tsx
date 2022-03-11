2103a43c984823873d2f15cd804e6c0a49ace4c9
tuliopxavier <tuliopxavier@gmail.com>
2022-03-11 06:34:17 -0300

    :zap: fix non work day fetch

--- src/service/getDolarPTAX.tsx
+++ src/service/getDolarPTAX.tsx
@@ -8,19 +8,27 @@ export const getDolarPTAX = () => {
   
   const current = new Date();
   const currentDate = `${current.getMonth()+1}/${current.getDate()}/${current.getFullYear()}`;
+  const lastDate = `${current.getMonth()+1}/${current.getDate()-1}/${current.getFullYear()}`;
+  const beforeLastDate = `${current.getMonth()+1}/${current.getDate()-2}/${current.getFullYear()}`;
 
   useEffect(() => {
       const fetchDolarPTAX = (date: string) => {
         fetch(`https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='${date}'&$top=100&$format=json&$select=cotacaoVenda`)
         .then(response => response.json())
-        .then(data => setDolarPTAX(data.value[0].cotacaoVenda));
+        .then(data => {
+          // IF contacaoVenda UNDEFINED, FETCH THE LAST VALID DAY (NULLISH COALESCING OPERATOR)
+          (data.value[0]) ? setDolarPTAX(data.value[0].cotacaoVenda) : (fetchLastDay() ?? fetchBeforeLastDay);
+        });
       };
+      
+      const fetchCurrentDay = () => {fetchDolarPTAX(currentDate)}
+      const fetchLastDay = ():any => {fetchDolarPTAX(lastDate)}
+      const fetchBeforeLastDay = () => {fetchDolarPTAX(beforeLastDate)}
+      
+      if (isSaturday(new Date(currentDate))) fetchLastDay();
+      if (isSunday(new Date(currentDate))) fetchBeforeLastDay();
+      fetchCurrentDay();
 
-      if (isSaturday(new Date(currentDate))){
-        fetchDolarPTAX(`${current.getMonth()+1}/${current.getDate()-1}/${current.getFullYear()}`);
-      } else if (isSunday(new Date(currentDate))) {
-        fetchDolarPTAX(`${current.getMonth()+1}/${current.getDate()-2}/${current.getFullYear()}`)
-      } else fetchDolarPTAX(`${current.getMonth()+1}/${current.getDate()}/${current.getFullYear()}`)
   }, [dolarPTAX]);
 
   return  dolarPTAX;
