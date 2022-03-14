abca9385ab2ef5f76e7a3b0c580985832aab90a5
tuliopxavier <tuliopxavier@gmail.com>
2022-03-14 04:06:18 -0300

    :bug: fix fetching dolar PTAX

--- src/service/getDolarPTAX.tsx
+++ src/service/getDolarPTAX.tsx
@@ -1,35 +1,24 @@
-import isSaturday from 'date-fns/isSaturday';
-import isSunday from 'date-fns/isSunday';
 import { useEffect, useState } from 'react';
-
 
 export const getDolarPTAX = () => {
   const [dolarPTAX, setDolarPTAX] = useState(0);
+  const currentDate = new Date();
   
-  const current = new Date();
-  const currentDate = `${current.getMonth()+1}/${current.getDate()}/${current.getFullYear()}`;
-  const lastDate = `${current.getMonth()+1}/${current.getDate()-1}/${current.getFullYear()}`;
-  const beforeLastDate = `${current.getMonth()+1}/${current.getDate()-2}/${current.getFullYear()}`;
-
   useEffect(() => {
-      const fetchDolarPTAX = (date: string) => {
-        fetch(`https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='${date}'&$top=100&$format=json&$select=cotacaoVenda`)
-        .then(response => response.json())
-        .then(data => {
-          // IF contacaoVenda UNDEFINED, FETCH THE LAST VALID DAY (NULLISH COALESCING OPERATOR)
-          (data.value[0]) ? setDolarPTAX(data.value[0].cotacaoVenda) : (fetchLastDay() ?? fetchBeforeLastDay);
-        });
-      };
-      
-      const fetchCurrentDay = () => {fetchDolarPTAX(currentDate)}
-      const fetchLastDay = ():any => {fetchDolarPTAX(lastDate)}
-      const fetchBeforeLastDay = () => {fetchDolarPTAX(beforeLastDate)}
-      
-      if (isSaturday(new Date(currentDate))) fetchLastDay();
-      if (isSunday(new Date(currentDate))) fetchBeforeLastDay();
-      fetchCurrentDay();
+    const fetchDolarPTAX = (dayBefore = 0) => {
+      const validDate = `${currentDate.getMonth()+1}/${currentDate.getDate()+dayBefore}/${currentDate.getFullYear()}`;
 
-  }, [dolarPTAX]);
+      fetch(`https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='${validDate}'&$top=100&$format=json&$select=cotacaoVenda`)
+      .then(response => response.json())
+      .then(data => {
+        // IF contacaoVenda IS UNDEFINED, FETCH THE DAY BEFORE
+        (data.value[0]) ? setDolarPTAX(data.value[0].cotacaoVenda) : (fetchDolarPTAX(dayBefore-1));
+      });
+    };
 
-  return  dolarPTAX;
+    fetchDolarPTAX();
+
+  }, []);
+
+  return dolarPTAX;
 };
